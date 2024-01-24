381d997af3079642e5ddb71840d31cc93d8a7706
Yangyue Wang <yangyuewang123@gmail.com>
2024-01-24 00:40:26 -0500

    Remove permalink

--- .eleventy.js
+++ .eleventy.js
@@ -196,7 +196,7 @@ module.exports = function (eleventyConfig) {
     breaks: true,
     linkify: true,
   }).use(markdownItAnchor, {
-    permalink: true,
+    permalink: false,
     permalinkClass: "direct-link",
     permalinkSymbol: "#",
   });
